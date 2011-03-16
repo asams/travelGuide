0fdcf2e0953d4213a1582975ecade283ab94bfdc
Erin Wuepper <ewuepper@mail.umw.edu>
2011-03-16 09:46:10 -0400

    added accountOverview and queried multiple tables

--- city.php
+++ city.php
@@ -13,7 +13,7 @@
 <?php
 	$cityID = $_GET['id'];
 	
-	$query = "SELECT * FROM cities WHERE city_id = $cityID";
+	$query = "SELECT ci.*, co.country_name, a.attraction_name, a.attraction_id FROM cities ci NATURAL JOIN countries co NATURAL JOIN attractions a WHERE city_id = $cityID";
 	$result = mysqli_query($db, $query) or die ("Error Querying Database - 1");
 	
 	while($row = mysqli_fetch_array($result)){
@@ -25,9 +25,14 @@
 		$flag = $row['city_flag'];
 		$coatOfArms = $row['city_coat_of_arms'];
 		$website = $row['city_website'];
+		$country_name = $row['country_name'];
+		$attractionName = $row['attraction_name'];
+		$attractionID = $row['attraction_id'];
+		
+		$attractionLinks = $attractionLinks . "<li><a href = \"attraction.php?id=" . $attractionID . "\">" . $attractionName . "</a></li>";
 	}
 	
-	$query = "SELECT attraction_name, attraction_id FROM attractions WHERE city_id = $cityID ORDER BY attraction_name";
+	/*$query = "SELECT attraction_name, attraction_id FROM attractions WHERE city_id = $cityID ORDER BY attraction_name";
 	
 	$result = mysqli_query($db, $query) or die ("Error Querying Database - 2");
 	
@@ -39,16 +44,16 @@
 		
 		$attractionLinks = $attractionLinks . "<li><a href = \"attraction.php?id=" . $attractionID . "\">" . $attractionName . "</a></li>";
 	}
-	
+	*/
 	$attractionLinks = $attractionLinks . "</ul>";
 
 ?>
 <?php
-	$query = "SELECT country_name FROM countries WHERE country_id = $countryID";
+	/*$query = "SELECT country_name FROM countries WHERE country_id = $countryID";
 	$result = mysqli_query($db, $query) or die ("Error Querying Database - 3");
 
 	$row = mysqli_fetch_array($result);
-	$country_name = $row['country_name'];
+	$country_name = $row['country_name'];*/
 	
 	echo "<h1>" . $cityName . "</h1>";
 
