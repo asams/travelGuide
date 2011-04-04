767fc1c79fbb48dc35cb97b69450808654ec8080
zeitz <rebecca.zeitz@gmail.com>
2011-04-04 17:00:01 -0400

    User comment capability on user uploaded city photos

--- photo.php
+++ photo.php
@@ -42,8 +42,28 @@ while($row = mysqli_fetch_array($result)) {
 	}
 }
 
+//get the city photo comments
+	$query = "SELECT cip.*, cp.photo_id, u.first_name, u.last_name FROM city_photos cp NATURAL JOIN city_photo_comments cip NATURAL JOIN users u WHERE cp.photo_id = $photoID ORDER BY cip.comment_date_submitted DESC";
+	$result = mysqli_query($db, $query) or die ("Error Querying Database - 2");
+
+	$comments_table = "<table rules = rows width = \"90%\">";
+	while($row = mysqli_fetch_array($result)){
+		$first_name = $row['first_name'];
+		$last_name = $row['last_name'];
+		$comment_subject = $row['comment_subject'];
+		$comment_body = $row['comment_body'];
+		$comment_date = $row['comment_date_submitted'];
+		
+		$comments_table = $comments_table . "<tr><td><br/>Name: " . $first_name . " " . $last_name . "<br/><br/>Subject: " . $comment_subject . "<br/><br/>Comment: " . $comment_body . "<br/><br/>Date: " . $comment_date . "<br/><br/></td></tr>";
+	}
+	$comments_table = $comments_table . "</table>";
+	
+	if ($comments_table <> "<table rules = rows width = \"90%\"></table>"){
+		echo "<H2></br></br></br>Comments from users:</H2>";
+		echo $comments_table;
+	}
 ?>
-<!-- </br></br></br>
+</br></br></br>
 <H2>Share your thoughts about <?php echo $subject ?>:</H2>
 <form action="cityPhotoCommentSubmitted.php" method="post" class="form">
 <center>
@@ -51,7 +71,7 @@ while($row = mysqli_fetch_array($result)) {
 
 <tr><th>Subject:</th><td><input type="text" id="subject" name="subject" size = 75 /></td></tr>
 <tr><th>Comment:</th><td><textarea name="comment" id="comment" rows = "4" cols = "60"></textarea>
-<input type="hidden" name="city_id" value=<?php echo $cityID ?>></td></tr>
+<input type="hidden" name="photo_id" value=<?php echo $photoID ?>></td></tr>
 
 <tr><td colspan = 2><center><input type="submit" class="formbutton" value="Submit" /></center></td></tr>
 </form>
@@ -59,7 +79,7 @@ while($row = mysqli_fetch_array($result)) {
 
 </center>
 </br></br>
--->
+
 <?php
 echo "</table></center>";		
 include('footer.php');
