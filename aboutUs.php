2877ecc225d6bea1ae30d9fd014677606c350f65
Amy Sams <asams@mail.umw.edu>
2011-04-28 19:53:31 -0400

    removed mean comments

--- aboutUs.php
+++ aboutUs.php
@@ -28,8 +28,7 @@ it to be added to our database, and then I'm sure granted no unforeseen circumst
 <tr><td width = "30%"><img src="AboutUsImages/amy.jpg" width = "100%" /></td><td><h2>Amy</h2><p>Amy is currently a senior at Mary Wash.
 	She is double majoring in Business Administration and Computer Science  (Computer Information Systems).  She doesn't know
 	what is in store for her future, but hopes that whatever job she gets is one that she loves (but more importantly...hopes
-	that she actually gets one).  She's never traveled outside of VA, unless W. VA counts....which it really doesn't.  On a side
-	note, she absolutely hates Kelsey (yes, it's spelled wrong but she doesn't care)...this project was torture for her*!</p></td></tr>
+	that she actually gets one).  She's never traveled outside of VA, unless W. VA counts....which it really doesn't.  </p></td></tr>
 <tr><td width = "30%"><img src="AboutUsImages/erin.jpg" width = "100%" /></td><td><h2>Erin</h2><p>Erin Wuepper is a Computer Science major set to graduate in the fall. Her father is in the military, so Erin
 	has had a lot of experience with travel. She lived in Germany for three years when she was in middle school and she went to high
 	school in Naples, Italy. She doesn't speak any foreign languages however, because she is a lazy bum. She would like to move to
@@ -38,14 +37,12 @@ it to be added to our database, and then I'm sure granted no unforeseen circumst
 	University of Mary Washington, double majoring in Math and German, and minoring in Computer Science.  Kelsie loves to travel,
 	and spent a semester studying in Munich, Germany.  She was able to finish her German major while there, but more importantly,
 	she traveled as much as possible and enjoyed every minute of it.  After graduation, Kelsie is hoping to get a job in the Math
-	or Computer Science fields, but hopes to travel as much as possible and visit Germany again as soon as possible.  (The real
-	reason Kelsie wants to leave is to get as far away from this Amy person as possible...but that's another story...*)</p></td></tr>
+	or Computer Science fields, but hopes to travel as much as possible and visit Germany again as soon as possible.  </p></td></tr>
 <tr><td width = "30%"><img src="AboutUsImages/rebecca.jpg" width = "100%" /></td><td><h2>Rebecca</h2><p>Ello.  I am a double major in Computer
 	Science and English with a concentration in creative writing.  My dream job would be to work for Pixar doing computer animation.
 	So, I love having trees around and that's what I marvel at when I travel.  For instance, my family went to Newport, Rhode Island
 	once a few summers ago to tour the mansions, which were gorgeous, but the best part for me was seeing all the trees, like the
-	Weeping European Beech.  They were amazing.  As a side note, or rather end note, I would advise you to disregard any previous
-	statements of hatred towards team members.</p></td></tr>
+	Weeping European Beech.  They were amazing.  </p></td></tr>
 </table>
 
 <!--echo "</td><td><img src = \"" . $map . "\" alt = \"map\" width = \"100%\" align = \"right\"/></td></tr></table>";
